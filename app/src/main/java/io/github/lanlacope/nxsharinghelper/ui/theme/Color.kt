a957f53fe750facf1ef53f91197be0de34739ef6
lanlacope <pokeuni150@gmail.com>
2024-06-21 06:40:02 +0900

    いくつかの色を追加

--- app/src/main/java/io/github/lanlacope/nxsharinghelper/ui/theme/Color.kt
+++ app/src/main/java/io/github/lanlacope/nxsharinghelper/ui/theme/Color.kt
@@ -9,3 +9,6 @@ val Pink80 = Color(0xFFEFB8C8)
 val Purple40 = Color(0xFF6650a4)
 val PurpleGrey40 = Color(0xFF625b71)
 val Pink40 = Color(0xFF7D5260)
+
+val BlueDark = Color(0xFF7FBFFF)
+val BlueLight = Color(0xFFA8D3FF)
