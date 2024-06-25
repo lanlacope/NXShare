64bb614d4a57bd196b9ed385cf45a8eed1733672
lanlacope <pokeuni150@gmail.com>
2024-06-25 15:26:17 +0900

    ハイパーリンク化

--- app/src/main/java/io/github/lanlacope/nxsharinghelper/activitys/LicenceActivity.kt
+++ app/src/main/java/io/github/lanlacope/nxsharinghelper/activitys/LicenceActivity.kt
@@ -1,17 +1,26 @@
 package io.github.lanlacope.nxsharinghelper.activitys
 
+import android.content.Intent
+import android.net.Uri
 import android.os.Bundle
-import android.text.Html
+import android.text.style.CharacterStyle
+import android.text.style.URLSpan
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.text.ClickableText
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Surface
-import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.remember
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.platform.LocalContext
 import androidx.compose.ui.res.stringResource
+import androidx.compose.ui.text.SpanStyle
+import androidx.compose.ui.text.buildAnnotatedString
+import androidx.compose.ui.text.style.TextDecoration
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.core.text.HtmlCompat
 import io.github.lanlacope.nxsharinghelper.R
 import io.github.lanlacope.nxsharinghelper.ui.theme.NXSharingHelperTheme
 
@@ -35,9 +44,54 @@ class LicenceActivity : ComponentActivity() {
 @Preview
 @Composable
 private fun LicenceView() {
-    Text(
-        text = stringResource(id = R.string.license),
-        modifier = Modifier
-            .fillMaxSize()
+    val context = LocalContext.current
+
+    val rawText = stringResource(id = R.string.license)
+    val linkColor = MaterialTheme.colorScheme.primary
+    val text = remember(rawText) {
+        val parsedText = HtmlCompat.fromHtml(
+            rawText.replace("\n", "<br>"),
+            HtmlCompat.FROM_HTML_MODE_COMPACT
+        )
+
+        return@remember buildAnnotatedString {
+            append(parsedText)
+
+            parsedText.getSpans(0, parsedText.length, CharacterStyle::class.java).forEach { span ->
+                if (span is URLSpan) {
+                    addStyle(
+                        style = SpanStyle(
+                            color = linkColor,
+                            textDecoration = TextDecoration.Underline
+                        ),
+                        start = parsedText.getSpanStart(span),
+                        end = parsedText.getSpanEnd(span)
+                    )
+                    addStringAnnotation(
+                        tag = "URL",
+                        annotation = span.url,
+                        start = parsedText.getSpanStart(span),
+                        end = parsedText.getSpanEnd(span)
+                    )
+                }
+            }
+        }
+    }
+
+    ClickableText(
+        text = text,
+        modifier = Modifier.fillMaxSize(),
+        onClick = {
+            text.getStringAnnotations(
+                tag = "URL",
+                start = it,
+                end = it
+            )
+                .firstOrNull()?.let {
+                    val uri = Uri.parse(it.item)
+                    val intent = Intent(Intent.ACTION_VIEW, uri)
+                    context.startActivity(intent)
+                }
+        }
     )
 }
