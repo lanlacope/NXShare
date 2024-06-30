c8b0e021c630bb8c6ee4fe81accbd514737c9eec
lanlacope <pokeuni150@gmail.com>
2024-06-30 21:56:29 +0900

    delete non-used

--- app/src/main/java/io/github/lanlacope/nxsharinghelper/States.kt
+++ app/src/main/java/io/github/lanlacope/nxsharinghelper/States.kt
@@ -1,7 +1,6 @@
 package io.github.lanlacope.nxsharinghelper
 
 import android.os.Build
-import androidx.lifecycle.viewmodel.viewModelFactory
 
 fun isAfterAndroidX(): Boolean {
     return Build.VERSION.SDK_INT >= Build.VERSION_CODES.Q
