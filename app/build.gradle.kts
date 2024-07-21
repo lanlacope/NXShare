7f03013cc61584f851950e19b7cee3ca8dc12efa
lanlacope <pokeuni150@gmail.com>
2024-07-21 15:52:02 +0900

    1.3-alpha-1

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -11,8 +11,8 @@ android {
         applicationId = "io.github.lanlacope.nxsharinghelper"
         minSdk = 23
         targetSdk = 34
-        versionCode = 121
-        versionName = "1.2-alpha-2"
+        versionCode = 130
+        versionName = "1.3-alpha-1"
 
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
         vectorDrawables {
