8b7cdc7f6e1f98c626f5717687d97305a81dd2bc
lanlacope <pokeuni150@gmail.com>
2024-12-07 16:35:24 +0900

    compileSdkの変更

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -5,7 +5,7 @@ plugins {
 
 android {
     namespace = "io.github.lanlacope.nxsharinghelper"
-    compileSdk = 34
+    compileSdk = 35
 
     defaultConfig {
         applicationId = "io.github.lanlacope.nxsharinghelper"
