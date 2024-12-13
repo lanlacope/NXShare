b7ac0d27f1bbedf4b77272dcfcca590ed92ba840
lanlacope <pokeuni150@gmail.com>
2024-12-14 00:01:23 +0900

    release 2.1.1

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -11,8 +11,8 @@ android {
         applicationId = "io.github.lanlacope.nxsharinghelper"
         minSdk = 23
         targetSdk = 35
-        versionCode = 21000
-        versionName = "2.1.0"
+        versionCode = 2001001
+        versionName = "2.1.1"
 
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
         vectorDrawables {
